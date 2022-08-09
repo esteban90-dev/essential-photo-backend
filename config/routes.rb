c9e85056597740c0b822cc9a2d4482502ac002e0
esteban90-dev <stephencallard@gmail.com>
2022-08-09 17:01:48 -0600

    add route for creating a new image

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       mount_devise_token_auth_for 'Admin', at: 'auth'
+
+      resources :images, only: :create
     end
   end
 end
