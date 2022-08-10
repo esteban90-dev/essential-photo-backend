0a1a7e6dee410926a92ddba46c2007813469128e
esteban90-dev <stephencallard@gmail.com>
2022-08-10 10:58:21 -0600

    create action creates an image record with an attached image

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,4 +1,21 @@
 class Api::V1::ImagesController < ApplicationController
   def create
+    @image = Image.new(image_params)
+    
+    if @image.save
+      render json: {
+        id: @image.id,
+        filename: @image.image.filename,
+        url: url_for(@image.image),
+        created_at: @image.created_at,
+        updated_at: @image.updated_at,
+      }, status: :created
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:image)
   end
 end
