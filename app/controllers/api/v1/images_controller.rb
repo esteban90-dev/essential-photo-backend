8856426fe01498a11942c82f1c1524af97cf23f6
esteban90-dev <stephencallard@gmail.com>
2022-09-14 11:36:00 -0600

    add update action to Images Controller

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -35,11 +35,38 @@ class Api::V1::ImagesController < ApplicationController
     end
   end
 
+  def update
+    @image = Image.find(params[:id])
+    @image.title = params[:title]
+    @image.description = params[:description]
+    @image.is_public = params[:is_public]
+
+    # for each tag from request, if the tag exists then associate
+    # with the image, otherwise create a new tag
+    params[:tags].split(", ").each do |tag_name|
+      tag = Tag.find_by(name: tag_name)
+      if tag
+        @image.tags << tag
+      else
+        @image.tags.create(name: tag_name)
+      end
+    end
+
+    if @image.save
+      render json: formatted_image(@image), status: :ok
+    else
+      render json: @image.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def formatted_image(image)
     return {
       id: image.id,
+      title: image.title,
+      description: image.description,
+      tags: image.tags,
       image_url: url_for(image.image),
       thumbnail_url: url_for(image.thumbnail),
       created_at: image.created_at,
