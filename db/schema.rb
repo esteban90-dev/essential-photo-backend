d4ccca461aa18ceb30153867ab94fa8d0c77aa9e
esteban90-dev <stephencallard@gmail.com>
2022-09-14 10:19:38 -0600

    create tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_14_160804) do
+ActiveRecord::Schema.define(version: 2022_09_14_161455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,12 @@ ActiveRecord::Schema.define(version: 2022_09_14_160804) do
     t.string "title"
     t.text "description"
     t.boolean "is_public"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
