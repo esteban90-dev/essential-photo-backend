46b8a06f3cb0ff9b2f06ff7e34d578eb15aff0a6
esteban90-dev <stephencallard@gmail.com>
2022-09-14 11:02:23 -0600

    create many to many relationship between images and tags

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ApplicationRecord
   has_one_attached :image
   has_one_attached :thumbnail
+  has_many :image_tags
+  has_many :tags, through: :image_tags
 
   validates :image, attached: true, content_type: [:png, :jpg, :jpeg]
 end
