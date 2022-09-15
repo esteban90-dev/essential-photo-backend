7a7e855217bdeb802db880402f4977f0334c7572
esteban90-dev <stephencallard@gmail.com>
2022-09-15 09:15:01 -0600

    deleting an image will destroy associated image_tag records

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   has_one_attached :image
   has_one_attached :thumbnail
-  has_many :image_tags
+  has_many :image_tags, dependent: :destroy
   has_many :tags, through: :image_tags
 
   validates :image, attached: true, content_type: [:png, :jpg, :jpeg]
